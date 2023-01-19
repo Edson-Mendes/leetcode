dc335ae276985e8a1542bc6338632ea59c926eff
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-01-19 15:29:38 -0300

    refact Longest Substring Without Repeating Characters

--- java/src/main/java/br/com/emendes/longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharacters.java
+++ java/src/main/java/br/com/emendes/longestsubstringwithoutrepeatingcharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -10,9 +10,51 @@ import java.util.Map;
 public class LongestSubstringWithoutRepeatingCharacters {
 
   /**
-   * Primeira solução que eu criei, acredito que execute em O(n²), então não está bom!
+   * Melhor solução que cheguei até o momento, executou em 5 ms.
    */
   public int lengthOfLongestSubstring(String s) {
+    char[] stringAsCharArray = s.toCharArray();
+    int arrayLength = stringAsCharArray.length;
+    int longest = 0;
+    int currentSize = 0;
+    int index = 0;
+    int indexStart = 0;
+    Integer indexFound;
+    Map<Character, Integer> actualCharacters = new HashMap<>();
+
+    while (index < arrayLength) {
+      indexFound = actualCharacters.get(stringAsCharArray[index]);
+      if (indexFound == null) {
+        currentSize++;
+      } else {
+        removeIndexes(stringAsCharArray, indexStart, indexFound, actualCharacters);
+        if (currentSize > longest) {
+          longest = currentSize;
+        }
+        currentSize = currentSize - (indexFound - indexStart);
+        indexStart = indexFound + 1;
+      }
+      actualCharacters.put(stringAsCharArray[index], index);
+      index++;
+    }
+
+    if (currentSize > longest) {
+      longest = currentSize;
+    }
+
+    return longest;
+  }
+
+  private void removeIndexes(char[] stringAsCharArray, int indexStart, int indexEnd, Map<Character, Integer> map) {
+    for (; indexStart < indexEnd; indexStart++) {
+      map.remove(stringAsCharArray[indexStart]);
+    }
+  }
+
+  /**
+   * Primeira solução que eu criei, acredito que execute em O(n²), então não está bom!
+   */
+  public int solucaoRuim(String s) {
     char[] stringAsCharArray = s.toCharArray();
     int arrayLength = stringAsCharArray.length;
     int longest = 0;
