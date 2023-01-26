490d7102b10ba98f6ac20115a719f4d3d6d6456e
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-01-26 17:35:32 -0300

    Median of two sorted arrays - solução aprimorada

--- java/src/main/java/br/com/emendes/medianoftwosortedarrays/MedianOfTwoSortedArrays.java
+++ java/src/main/java/br/com/emendes/medianoftwosortedarrays/MedianOfTwoSortedArrays.java
@@ -2,12 +2,67 @@ package br.com.emendes.medianoftwosortedarrays;
 
 import java.util.Arrays;
 
+/**
+ * Dadas dois arrays ordenados nums1 e nums2 de tamanho m e n respectivamente, retorne a mediana dos dois arrays.
+ */
 public class MedianOfTwoSortedArrays {
 
   public double findMedianSortedArrays(int[] nums1, int[] nums2) {
     int nums1Length = nums1.length;
     int nums2Length = nums2.length;
-    int[] mergedArray = merge(nums1, nums2, nums1Length, nums2Length); // merge os dois arrays.
+
+    int[] mergedArray = merge(nums1, nums2, nums1Length, nums2Length); // merge os dois arrays tipo o merge do MergeSort
+
+    if ((nums1Length+nums2Length) % 2 == 0) { // verificar se o novo tamanho é par
+      // se sim, soma os dois valores centrais e divide por 2.
+      int centralValue = (nums1Length+nums2Length) / 2;
+      double median1 = mergedArray[centralValue];
+      double median2 = mergedArray[centralValue - 1];
+      return (median1 + median2)/2.0;
+    }
+    // se não, retorna o valor central
+    return mergedArray[(nums1Length+nums2Length) / 2];
+  }
+
+  private int[] merge(int[] nums1, int[] nums2, int length1, int length2) {
+    int[] newArray = new int[length1 + length2];
+
+    int indexNum1 = 0;
+    int indexNum2 = 0;
+    int indexNewArray = 0;
+
+    while (indexNum1 < length1 && indexNum2 < length2) {
+      if (nums2[indexNum2] < nums1[indexNum1]) {
+        newArray[indexNewArray] = nums2[indexNum2];
+        indexNum2++;
+      } else {
+        newArray[indexNewArray] = nums1[indexNum1];
+        indexNum1++;
+      }
+      indexNewArray++;
+    }
+
+    while (indexNum1 < length1) {
+      newArray[indexNewArray] = nums1[indexNum1];
+      indexNewArray++;
+      indexNum1++;
+    }
+
+    while (indexNum2 < length2) {
+      newArray[indexNewArray] = nums2[indexNum2];
+      indexNewArray++;
+      indexNum2++;
+    }
+
+    return newArray;
+  }
+
+  // Abaixo é a solução antiga
+  @Deprecated
+  public double oldSolution(int[] nums1, int[] nums2) {
+    int nums1Length = nums1.length;
+    int nums2Length = nums2.length;
+    int[] mergedArray = oldMerge(nums1, nums2, nums1Length, nums2Length); // merge os dois arrays.
 
     Arrays.sort(mergedArray); // Ordenar o novo array.
 
@@ -22,7 +77,7 @@ public class MedianOfTwoSortedArrays {
     return mergedArray[(nums1Length+nums2Length) / 2];
   }
 
-  private int[] merge(int[] nums1, int[] nums2, int length1, int length2) {
+  private int[] oldMerge(int[] nums1, int[] nums2, int length1, int length2) {
     int[] mergedArray = Arrays.copyOf(nums1, length1 + length2);
     int i = 0;
 
