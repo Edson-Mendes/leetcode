a3a97edbab2513e66ca011fc9be0fe5890c93429
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-01-26 18:00:35 -0300

    Palindrome Number - solução um pouco melhor

--- java/src/main/java/br/com/emendes/palindromenumber/PalindromeNumber.java
+++ java/src/main/java/br/com/emendes/palindromenumber/PalindromeNumber.java
@@ -5,8 +5,23 @@ package br.com.emendes.palindromenumber;
  */
 public class PalindromeNumber {
 
-  // Solução inicial, não executou em um bom tempo (16ms).
+  // Solução um pouco melhor, executou em 11ms, mas ainda tem solução melhor.
   public boolean isPalindrome(int x) {
+    if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+
+    long reversedX = 0;
+    long tempX = x;
+
+    while (x / 10 != 0) {
+      reversedX = (reversedX * 10) + x % 10;
+      x = x / 10;
+    }
+    reversedX = (reversedX * 10) + x % 10;
+    return tempX == reversedX;
+  }
+
+  // Solução inicial, não executou em um bom tempo (16ms).
+  public boolean oldSolution(int x) {
     if (x < 0) return false; // Número negativo não pode ser palíndromo por causa do sinal.
 
     char[] number = String.valueOf(x).toCharArray();
@@ -14,7 +29,7 @@ public class PalindromeNumber {
     int rightIndex = number.length - 1;
 
     while (leftIndex < rightIndex) {
-      if(number[leftIndex] != number[rightIndex]) return false;
+      if (number[leftIndex] != number[rightIndex]) return false;
 
       leftIndex++;
       rightIndex--;
@@ -26,7 +41,7 @@ public class PalindromeNumber {
   public static void main(String[] args) {
     PalindromeNumber solution = new PalindromeNumber();
 
-    int x = 101;
+    int x = 1111111111;
 
     System.out.println(solution.isPalindrome(x));
   }
