eca8f057cf52acdcf21cda0d9d3698703c6fa6b3
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-02-20 08:51:19 -0300

    4Sum - Solução melhorada

--- java/src/main/java/br/com/emendes/foursum/FourSum.java
+++ java/src/main/java/br/com/emendes/foursum/FourSum.java
@@ -18,9 +18,69 @@ import java.util.List;
  */
 public class FourSum {
 
+  // Solução em 3ms, porém mais complexa que a versão anterior.
+  public List<List<Integer>> fourSum(int[] nums, int target) {
+    List<List<Integer>> answer = new ArrayList<>();
+    int numsLength = nums.length;
+
+    if (numsLength < 4) return answer;
+
+    int a;
+    int b;
+    int c;
+    int d;
+    long sum;
+    long aPlusB;
+
+    Arrays.sort(nums);
+
+    long last2 = nums[numsLength - 1] + nums[numsLength - 2];
+    long last3 = last2 + nums[numsLength - 3];
+
+    for (a = 0; a < numsLength - 3; a++) {
+
+      if ((long) nums[a] + nums[a + 1] + nums[a + 2] + nums[a + 3] > target) break;
+      if (nums[a] + last3 < target) continue;
+
+      if (a == 0 || nums[a] != nums[a - 1]) {
+        for (b = a + 1; b < numsLength - 2; b++) {
+
+          aPlusB = nums[a] + nums[b];
+          if (aPlusB + nums[b + 1] + nums[b + 2] > target) break;
+          if (aPlusB + last2 < target) continue;
+
+          if (b == a + 1 || nums[b] != nums[b - 1]) {
+            c = b + 1;
+            d = numsLength - 1;
+            while (c < d) {
+              sum = aPlusB + (long) nums[c] + nums[d];
+              if (sum < target) {
+                c++;
+              } else if (sum > target) {
+                d--;
+              } else {
+                answer.add(List.of(nums[a], nums[b], nums[c], nums[d]));
+
+                c++;
+                d--;
+                while (c < d && nums[c] == nums[c - 1]) c++;
+                while (c < d && nums[d] == nums[d + 1]) d--;
+              }
+            }
+          }
+
+        }
+      }
+
+
+    }
+
+    return answer;
+  }
+
   // Solução em 19 ms.
   // Adptaçao do 3Sum.
-  public List<List<Integer>> fourSum(int[] nums, int target) {
+  public List<List<Integer>> oldSolution(int[] nums, int target) {
     List<List<Integer>> answer = new ArrayList<>();
     int a;
     int b;
