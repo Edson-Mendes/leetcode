5052561b97d3dedc112209aeb8c5eaf5165c1596
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-05-18 08:03:17 -0300

    Find first and last position of element in sorted array - Solução em O(log N)

--- java/src/main/java/br/com/emendes/problems/FindFirstAndLastPositionOfElementInSortedArray.java
+++ java/src/main/java/br/com/emendes/problems/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -21,8 +21,9 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
   /**
    * Complexidade de tempo:<br>
    * O(log n) para buscar target no array nums.<br>
-   * O(n) para buscar as pontas esquerda e direira.<br>
-   * Então complexidade de tempo O(n + log n)
+   * O(log n) para buscar as ponta esquerda.<br>
+   * O(log n) para buscar as ponta direita.<br>
+   * Então complexidade de tempo O(3 * log n), logo O(log n).
    */
   public int[] searchRange(int[] nums, int target){
     int numsLength = nums.length;
@@ -34,20 +35,62 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
 
     if (index < 0) return answer;
 
-    int left = index - 1;
-    int right = index + 1;
-
-    while (left > -1 && nums[left] == target) {
-      left--;
-    }
-    while (right < numsLength && nums[right] == target) {
-      right++;
-    }
-
-    answer[0] = left + 1;
-    answer[1] = right - 1;
+    answer[0] = startsAt(nums, target, index);
+    answer[1] = endsAt(nums, target, index);
 
     return answer;
+  }
+
+  /**
+   * Busca o índice que contém target mais a esquerda do array nums.
+   * @param nums array ordenado.
+   * @param target valor a ser buscado.
+   * @param seenAt algum índice do array nums que contém target.
+   * @return O índice mais a esquerda que contém target.
+   */
+  private int startsAt(int[] nums, int target, int seenAt) {
+    int end = seenAt - 1;
+    int start = 0;
+    int mid;
+    int leftIndex = seenAt;
+
+    while (start <= end) {
+      mid = (end - start)/2 + start;
+      if (nums[mid] == target) {
+        leftIndex = mid;
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+
+    return leftIndex;
+  }
+
+  /**
+   * Busca o índice que contém target mais a direita do array nums.
+   * @param nums array ordenado.
+   * @param target valor a ser buscado.
+   * @param seenAt algum índice do array nums que contém target.
+   * @return O índice mais a direita que contém target.
+   */
+  private int endsAt(int[] nums, int target, int seenAt) {
+    int end = nums.length - 1;
+    int start = seenAt + 1;
+    int mid;
+    int rightIndex = seenAt;
+
+    while (start <= end) {
+      mid = (end - start)/2 + start;
+      if (nums[mid] == target) {
+        rightIndex = mid;
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+
+    return rightIndex;
   }
 
 }
