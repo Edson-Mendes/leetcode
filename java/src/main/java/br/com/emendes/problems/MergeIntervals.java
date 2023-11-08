c5d801f3b9b36c7148e6504434a54e34fa140efc
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-11-08 08:02:58 -0300

    Merge Intervals

--- java/src/main/java/br/com/emendes/problems/MergeIntervals.java
+++ java/src/main/java/br/com/emendes/problems/MergeIntervals.java
@@ -1,9 +1,9 @@
 package br.com.emendes.problems;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Dado um array intervals, onde intervals[i] = [startI, endI], mesclar todos os intervalos sobrepostos,
@@ -19,69 +19,20 @@ public class MergeIntervals {
   public int[][] merge(int[][] intervals) {
     if (intervals.length == 1) return intervals;
 
-    Map<Integer, int[]> intervalMap = mergeIntervals(intervals);
-    int[][] answer = mapToArray(intervalMap);
-    int intervalLengthBefore = intervals.length;
+    Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
-    while (answer.length < intervalLengthBefore) {
-      intervalLengthBefore = answer.length;
-
-      intervalMap = mergeIntervals(answer);
-      answer = mapToArray(intervalMap);
-    }
-
-    return answer;
-  }
-
-  private int[][] mapToArray(Map<Integer, int[]> intervalMap) {
-    Set<int[]> intervalSet = new HashSet<>(intervalMap.values());
-
-    int[][] newIntervals = new int[intervalSet.size()][2];
-    int i = 0;
-    for (int[] interval : intervalSet) {
-      newIntervals[i] = interval;
-      i++;
-    }
-
-    return newIntervals;
-  }
-
-  private Map<Integer, int[]> mergeIntervals(int[][] intervals) {
-    Map<Integer, int[]> intervalMap = new HashMap<>();
+    List<int[]> answer = new ArrayList<>();
 
     for (int[] interval : intervals) {
-      int[] overlappingInterval = intervalMap.get(interval[0]);
-      if (overlappingInterval != null) {
-        merge(overlappingInterval, interval);
-        for (int i = interval[1]; i > interval[0]; i--) {
-          intervalMap.put(i, overlappingInterval);
-        }
+      if (answer.isEmpty() || interval[0] > answer.get(answer.size() - 1)[1]) {
+        answer.add(interval);
       } else {
-        overlappingInterval = intervalMap.get(interval[1]);
-        if (overlappingInterval != null) {
-          merge(overlappingInterval, interval);
-          for (int i = interval[0]; i < interval[1]; i++) {
-            intervalMap.put(i, overlappingInterval);
-          }
-        } else {
-          int[] newInterval = {interval[0], interval[1]};
-          for (int i = interval[0]; i <= interval[1]; i++) {
-            intervalMap.put(i, newInterval);
-          }
-        }
+        int[] lastInterval = answer.get(answer.size() - 1);
+        lastInterval[1] = Math.max(lastInterval[1], interval[1]);
       }
     }
 
-    return intervalMap;
-  }
-
-  private void merge(int[] overlappingInterval, int[] interval) {
-    if (interval[0] < overlappingInterval[0]) {
-      overlappingInterval[0] = interval[0];
-    }
-    if (interval[1] > overlappingInterval[1]) {
-      overlappingInterval[1] = interval[1];
-    }
+    return answer.toArray(int[][]::new);
   }
 
 }
