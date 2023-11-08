b5dc1a3f22ed500bbb673f9dd326cb23ef63e4bc
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-11-08 14:23:04 -0300

    Merge Intervals - other solution

--- java/src/main/java/br/com/emendes/problems/MergeIntervals.java
+++ java/src/main/java/br/com/emendes/problems/MergeIntervals.java
@@ -1,9 +1,6 @@
 package br.com.emendes.problems;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Dado um array intervals, onde intervals[i] = [startI, endI], mesclar todos os intervalos sobrepostos,
@@ -33,6 +30,39 @@ public class MergeIntervals {
     }
 
     return answer.toArray(int[][]::new);
+  }
+
+  public int[][] otherSolution(int[][] intervals) {
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
+
+    for (int[] interval : intervals) {
+      min = Math.min(min, interval[0]);
+      max = Math.max(max, interval[0]);
+    }
+
+    int[] range = new int[max - min + 1];
+    for (int[] interval : intervals) {
+      range[interval[0] - min] = Math.max(interval[1] - min, range[interval[0] - min]);
+    }
+
+    int start = 0;
+    int end = 0;
+    List<int[]> result = new LinkedList<>();
+    for (int i = 0; i < range.length; i++) {
+      if (range[i] == 0) {
+        continue;
+      }
+      if (i <= end) {
+        end = Math.max(range[i], end);
+      } else {
+        result.add(new int[]{start + min, end + min});
+        start = i;
+        end = range[i];
+      }
+    }
+    result.add(new int[]{start + min, end + min});
+    return result.toArray(new int[result.size()][]);
   }
 
 }
