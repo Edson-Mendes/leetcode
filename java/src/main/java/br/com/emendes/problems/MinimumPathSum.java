2c55169e3f9f2cc6fb76adeef981bb8d102313c7
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-11-27 11:02:13 -0300

    Minimum Path Sum - segunda solução

--- java/src/main/java/br/com/emendes/problems/MinimumPathSum.java
+++ java/src/main/java/br/com/emendes/problems/MinimumPathSum.java
@@ -41,4 +41,26 @@ public class MinimumPathSum {
     return buffer[m - 1][n - 1];
   }
 
+  /**
+   * Solução em que é alterado o grid de entrada.
+   */
+  public int secondSolution(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i > 0 && j > 0) {
+          grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+        } else if (i > 0) {
+          grid[i][j] += grid[i - 1][j];
+        } else if (j > 0) {
+          grid[i][j] += grid[i][j - 1];
+        }
+      }
+    }
+
+    return grid[m - 1][n - 1];
+  }
+
 }
