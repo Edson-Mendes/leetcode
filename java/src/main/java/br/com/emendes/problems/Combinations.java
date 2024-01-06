5a0f12e734ee83281d3c1f4e8cb81d0197952c04
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-01-06 17:21:28 -0300

    Combinations - refact

--- java/src/main/java/br/com/emendes/problems/Combinations.java
+++ java/src/main/java/br/com/emendes/problems/Combinations.java
@@ -14,28 +14,22 @@ import java.util.List;
  */
 public class Combinations {
 
-  private List<List<Integer>> answer;
-
   public List<List<Integer>> combine(int n, int k) {
-    if (n == 1) {
-      return List.of(List.of(1));
-    }
-    answer = new ArrayList<>();
-
-    for (int i = 1; i <= n - k + 1; i++) {
-      combine(i, n, k, new ArrayList<>());
-    }
+    List<List<Integer>> answer = new ArrayList<>();
+    combine(n, k, 1, new ArrayList<>(), answer);
     return answer;
   }
 
-  private void combine(int value, int n, int k, List<Integer> list) {
-    list.add(value);
+  private void combine(int n, int k, int value, List<Integer> list, List<List<Integer>> answer) {
     if (list.size() == k) {
-      answer.add(list);
+      answer.add(new ArrayList<>(list));
       return;
     }
-    for (int newValue = value + 1; n - newValue + 1 >= k - list.size(); newValue++) {
-      combine(newValue, n, k, new ArrayList<>(list));
+
+    for (int i = value; i <= n; i++) {
+      list.add(i);
+      combine(n, k, i + 1, list, answer);
+      list.remove(list.size() - 1);
     }
   }
 
