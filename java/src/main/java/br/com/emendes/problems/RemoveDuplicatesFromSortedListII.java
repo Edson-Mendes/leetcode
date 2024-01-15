2f3e7f7252045edc6baf7aed652c9a0f37acf24f
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-01-15 19:48:27 -0300

    Remove Duplicates from Sorted List II - refact

--- java/src/main/java/br/com/emendes/problems/RemoveDuplicatesFromSortedListII.java
+++ java/src/main/java/br/com/emendes/problems/RemoveDuplicatesFromSortedListII.java
@@ -14,6 +14,25 @@ import br.com.emendes.problems.util.ListNode;
 public class RemoveDuplicatesFromSortedListII {
 
   public ListNode deleteDuplicates(ListNode head) {
+    ListNode dummy = new ListNode(0, head);
+    ListNode prev = dummy;
+
+    while (head != null) {
+      while (head.next != null && head.val == head.next.val) {
+        head = head.next;
+      }
+      if (prev.next == head) {
+        prev = prev.next;
+      } else {
+        prev.next = head.next;
+      }
+      head = head.next;
+    }
+
+    return dummy.next;
+  }
+
+  public ListNode firstSolution(ListNode head) {
     if (head == null || head.next == null) return head;
     ListNode start = head;
 
