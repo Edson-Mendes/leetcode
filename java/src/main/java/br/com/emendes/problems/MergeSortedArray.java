6db598059610afa5d3d41f457c49b127d13b4eb1
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-02-06 18:27:08 -0300

    Merge sorted array - refact

--- java/src/main/java/br/com/emendes/problems/MergeSortedArray.java
+++ java/src/main/java/br/com/emendes/problems/MergeSortedArray.java
@@ -22,37 +22,17 @@ package br.com.emendes.problems;
 public class MergeSortedArray {
 
   public void merge(int[] nums1, int m, int[] nums2, int n) {
-    if (n == 0) return;
-    if (m == 0) {
-      System.arraycopy(nums2, 0, nums1, 0, n);
-      return;
-    }
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
 
-    int[] numsAux = new int[m + n];
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    while (i < m && j < n) {
-      if (nums2[j] < nums1[i]) {
-        numsAux[k] = nums2[j];
-        j++;
+    while (j >= 0) {
+      if (i >= 0 && nums1[i] > nums2[j]) {
+        nums1[k--] = nums1[i--];
       } else {
-        numsAux[k] = nums1[i];
-        i++;
+        nums1[k--] = nums2[j--];
       }
-      k++;
     }
-    while (i < m) {
-      numsAux[k] = nums1[i];
-      i++;
-      k++;
-    }
-    while (j < n) {
-      numsAux[k] = nums2[j];
-      j++;
-      k++;
-    }
-    System.arraycopy(numsAux, 0, nums1, 0, numsAux.length);
   }
 
 }
