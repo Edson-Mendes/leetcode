537b0920e550a79719f1298f1786decbdab85aa6
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-02-21 17:45:43 -0300

    Interleaving String - com array 2D

--- java/src/main/java/br/com/emendes/problems/InterleavingString.java
+++ java/src/main/java/br/com/emendes/problems/InterleavingString.java
@@ -22,6 +22,29 @@ import java.util.Arrays;
 public class InterleavingString {
 
   public boolean isInterleave(String s1, String s2, String s3) {
+    final int m = s1.length();
+    final int n = s2.length();
+    if (m + n != s3.length())
+      return false;
+
+    boolean[][] dp = new boolean[m + 1][n + 1];
+    dp[0][0] = true;
+
+    for (int i = 1; i <= m; i++)
+      dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+
+    for (int j = 1; j <= n; j++)
+      dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+
+    for (int i = 1; i <= m; i++)
+      for (int j = 1; j <= n; j++)
+        dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1) ||
+            dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+
+    return dp[m][n];
+  }
+
+  public boolean firstSolution(String s1, String s2, String s3) {
     if (s3.length() != s1.length() + s2.length()) return false;
 
     int[][][] dp = new int[s1.length() + 1][s2.length() + 1][s3.length()];
@@ -32,19 +55,19 @@ public class InterleavingString {
       }
     }
 
-    return isInterleave(s1, s2, s3, 0, 0, 0, dp);
+    return firstSolutionHelper(s1, s2, s3, 0, 0, 0, dp);
   }
 
-  private boolean isInterleave(String s1, String s2, String s3, int s1Index, int s2Index, int s3Index, int[][][] dp) {
+  private boolean firstSolutionHelper(String s1, String s2, String s3, int s1Index, int s2Index, int s3Index, int[][][] dp) {
     if (s3Index == s3.length()) return true;
     if (dp[s1Index][s2Index][s3Index] != -1) return dp[s1Index][s2Index][s3Index] == 1;
 
     boolean answer = false;
     if (s1Index < s1.length() && s1.charAt(s1Index) == s3.charAt(s3Index)) {
-      answer = isInterleave(s1, s2, s3, s1Index + 1, s2Index, s3Index + 1, dp);
+      answer = firstSolutionHelper(s1, s2, s3, s1Index + 1, s2Index, s3Index + 1, dp);
     }
     if (!answer && s2Index < s2.length() && s2.charAt(s2Index) == s3.charAt(s3Index)) {
-      answer = isInterleave(s1, s2, s3, s1Index, s2Index + 1, s3Index + 1, dp);
+      answer = firstSolutionHelper(s1, s2, s3, s1Index, s2Index + 1, s3Index + 1, dp);
     }
     dp[s1Index][s2Index][s3Index] = answer ? 1 : 0;
     return answer;
