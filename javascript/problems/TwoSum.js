8e68c50a318b8ed4d09b545865debc2974f63a2b
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-03-01 21:20:54 -0300

    javascript: Two Sum - solution using map

--- javascript/problems/TwoSum.js
+++ javascript/problems/TwoSum.js
@@ -19,12 +19,20 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const value = target - nums[i];
+
+    if (value in map) return [i, map[value]];
+
+    map[nums[i]] = i;
+  }
+};
+
+var firstSolution = function (nums, target) {
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] == target) return [i, j];
     }
   }
 };
-
-console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSum([3, 2, 4], 6));
