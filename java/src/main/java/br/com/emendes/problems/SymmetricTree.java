957d7a30f9bb9695d6d54b23384a121f5e1d4388
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-03-05 10:51:27 -0300

    java: Symmetric Tree - Solução iterativa

--- java/src/main/java/br/com/emendes/problems/SymmetricTree.java
+++ java/src/main/java/br/com/emendes/problems/SymmetricTree.java
@@ -2,6 +2,9 @@ package br.com.emendes.problems;
 
 import br.com.emendes.problems.util.TreeNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Dado o root de uma bynary tree, verifique se é espelhado (simétrico em volta do centro).<br>
  * <br>
@@ -13,10 +16,56 @@ import br.com.emendes.problems.util.TreeNode;
  */
 public class SymmetricTree {
 
+  /**
+   * Solução iterativa.
+   */
   public boolean isSymmetric(TreeNode root) {
+    if (root.left == null || root.right == null) return root.left == root.right;
+
+    Deque<TreeNode> stack1 = new LinkedList<>();
+    Deque<TreeNode> stack2 = new LinkedList<>();
+
+    stack1.push(root.left);
+    stack2.push(root.right);
+
+    while (!stack1.isEmpty() && !stack2.isEmpty()) {
+      TreeNode node1 = stack1.pop();
+      TreeNode node2 = stack2.pop();
+
+      if (!isEquals(node1, node2)) return false;
+
+      if (node1 != null) {
+        stack1.push(node1.left);
+        stack1.push(node1.right);
+      }
+      if (node2 != null) {
+        stack2.push(node2.right);
+        stack2.push(node2.left);
+      }
+    }
+
+    return stack1.isEmpty() && stack2.isEmpty();
+  }
+
+  /**
+   * Usado no método {@link SymmetricTree#isSymmetric(TreeNode)}.
+   */
+  private boolean isEquals(TreeNode node1, TreeNode node2) {
+    if (node1 == null && node2 == null) return true;
+    if (node1 == null || node2 == null) return false;
+    return node1.val == node2.val;
+  }
+
+  /**
+   * Solução recursiva.
+   */
+  public boolean recursivelySolution(TreeNode root) {
     return isSymmetric(root.left, root.right);
   }
 
+  /**
+   * Usado no método {@link SymmetricTree#recursivelySolution(TreeNode)}.
+   */
   private boolean isSymmetric(TreeNode node1, TreeNode node2) {
     if (node1 == null && node2 == null) return true;
     if (node1 == null || node2 == null) return false;
