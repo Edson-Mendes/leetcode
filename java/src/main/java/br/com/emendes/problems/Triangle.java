61b36a34197746f87998a0772f450dabba027d5b
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-04-03 14:05:59 -0300

    java: Triangle - second solution

--- java/src/main/java/br/com/emendes/problems/Triangle.java
+++ java/src/main/java/br/com/emendes/problems/Triangle.java
@@ -20,22 +20,35 @@ public class Triangle {
     int rows = triangle.size();
     if (rows == 1) return triangle.get(0).get(0);
 
+    int[] previousRow = {triangle.get(0).get(0)};
     for (int i = 1; i < rows; i++) {
-      int rowLength = i + 1;
-      List<Integer> previousRow = triangle.get(i - 1);
-      List<Integer> currentRow = triangle.get(i);
-
-      int value = currentRow.remove(0) + previousRow.get(0);
-      currentRow.add(0, value);
-      for (int j = 1; j < rowLength - 1; j++) {
-        value = currentRow.remove(j) + Math.min(previousRow.get(j - 1), previousRow.get(j));
-        currentRow.add(j, value);
-      }
-      value = currentRow.remove(rowLength - 1) + previousRow.get(rowLength - 2);
-      currentRow.add(value);
+      previousRow = calcRow(previousRow, triangle.get(i), i + 1);
     }
 
-    return triangle.get(rows - 1).stream().min(Integer::compareTo).orElseThrow();
+    int answer = Integer.MAX_VALUE;
+
+    for (int value : previousRow) {
+      if (value < answer) {
+        answer = value;
+      }
+    }
+
+    return answer;
+  }
+
+  private int[] calcRow(int[] previousRow, List<Integer> currRow, int rowLength) {
+    int[] result = new int[rowLength];
+
+    int value = currRow.get(0) + previousRow[0];
+    result[0] = value;
+
+    for (int i = 1; i < rowLength - 1; i++) {
+      result[i] = currRow.get(i) + Math.min(previousRow[i - 1], previousRow[i]);
+    }
+
+    value = currRow.get(rowLength - 1) + previousRow[rowLength - 2];
+    result[rowLength - 1] = value;
+    return result;
   }
 
 }
