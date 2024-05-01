a6a26be207c4374883903b4fe93db24df28563e0
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-04-30 21:13:20 -0300

    java: Candy - second solution

--- java/src/main/java/br/com/emendes/problems/Candy.java
+++ java/src/main/java/br/com/emendes/problems/Candy.java
@@ -1,5 +1,7 @@
 package br.com.emendes.problems;
 
+import java.util.Arrays;
+
 /**
  * Há n crianças em fila. Cada criança recebe um valor de classificação fornecido no array de inteiros ratings.<br>
  * <br>
@@ -20,6 +22,34 @@ package br.com.emendes.problems;
 public class Candy {
 
   public int candy(int[] ratings) {
+    int[] startToEnd = new int[ratings.length];
+    int[] endToStart = new int[ratings.length];
+    int answer = 0;
+
+    Arrays.fill(startToEnd, 1);
+    Arrays.fill(endToStart, 1);
+
+    for (int i = 1; i < ratings.length; i++) {
+      if (ratings[i] > ratings[i - 1]) {
+        startToEnd[i] = startToEnd[i - 1] + 1;
+      }
+    }
+    for (int i = ratings.length - 2; i >= 0; i--) {
+      if (ratings[i] > ratings[i + 1]) {
+        endToStart[i] = endToStart[i + 1] + 1;
+      }
+    }
+    for (int i = 0; i < ratings.length; i++) {
+      answer += Math.max(startToEnd[i], endToStart[i]);
+    }
+
+    return answer;
+  }
+
+  /**
+   * First solution.
+   */
+  public int firstSolution(int[] ratings) {
     int candyCounter = 1;
     int[] cache = new int[ratings.length];
     cache[0] = 1;
