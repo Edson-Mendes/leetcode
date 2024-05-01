ae2b855ddaeeed88e4d39c2d95c20a1e49411c49
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-05-01 11:27:47 -0300

    java: Single Number - best solution

--- java/src/main/java/br/com/emendes/problems/SingleNumber.java
+++ java/src/main/java/br/com/emendes/problems/SingleNumber.java
@@ -11,16 +11,29 @@ import java.util.Arrays;
  * Restrições:<br>
  * <ul>
  *   <li>1 <= nums.length <= 3 * 10⁴</li>
- *   <li>-3 * 104 <= nums[i] <= 3 * 10⁴</li>
+ *   <li>-3 * 10⁴ <= nums[i] <= 3 * 10⁴</li>
  *   <li>Cada elemento da matriz aparece duas vezes, exceto um elemento que aparece apenas uma vez.</li>
  * </ul>
  */
 public class SingleNumber {
 
   public int singleNumber(int[] nums) {
+    int answer = 0;
+
+    for (int value : nums) {
+      answer ^= value;
+    }
+
+    return answer;
+  }
+
+  /**
+   * First solution.
+   */
+  public int firstSolution(int[] nums) {
     Arrays.sort(nums);
 
-    for (int i = 1; i < nums.length; i+=2) {
+    for (int i = 1; i < nums.length; i += 2) {
       if (nums[i] != nums[i - 1]) return nums[i - 1];
     }
 
