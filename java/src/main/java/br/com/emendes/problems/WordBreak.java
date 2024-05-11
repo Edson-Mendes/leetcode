df62774160d27552d07a17baaf76b6d86edb9a22
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-05-11 10:59:11 -0300

    java: Word Break - second solution

--- java/src/main/java/br/com/emendes/problems/WordBreak.java
+++ java/src/main/java/br/com/emendes/problems/WordBreak.java
@@ -1,9 +1,6 @@
 package br.com.emendes.problems;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -23,10 +20,38 @@ import java.util.stream.Collectors;
  */
 public class WordBreak {
 
+  public boolean wordBreak(String s, List<String> wordDict) {
+    Set<String> wordSet = new HashSet<>(wordDict);
+    int maxLength = getMaxLength(wordDict);
+    int length = s.length();
+    boolean[] dp = new boolean[length + 1];
+    dp[0] = true;
+
+    for (int i = 1; i <= length; i++) {
+      for (int j = i - 1; j >= 0; j--) {
+        if (i - j > maxLength) break;
+        if (dp[j] && wordSet.contains(s.substring(j, i))) {
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+
+    return dp[length];
+  }
+
+  private int getMaxLength(List<String> wordDict) {
+    return wordDict.stream().mapToInt(String::length).max().getAsInt();
+  }
+
+  // ------------------ First Solution ------------------
   private Set<String> dict;
   private Map<String, Boolean> cache;
 
-  public boolean wordBreak(String s, List<String> wordDict) {
+  /**
+   * First Solution.
+   */
+  public boolean firstSolution(String s, List<String> wordDict) {
     dict = wordDict.stream().collect(Collectors.toSet());
     cache = new HashMap<>();
     return canSegment(s);
