c4caea116484f66587e5cde1165deacca2e8ec73
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-05-27 19:28:26 -0300

    java: Linked List Cycle II - second solution

--- java/src/main/java/br/com/emendes/problems/LinkedListCycleII.java
+++ java/src/main/java/br/com/emendes/problems/LinkedListCycleII.java
@@ -26,6 +26,27 @@ import java.util.Set;
 public class LinkedListCycleII {
 
   public ListNode detectCycle(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+
+      if (fast == slow) {
+        slow = head;
+        while (slow != fast) {
+          slow = slow.next;
+          fast = fast.next;
+        }
+        return slow;
+      }
+    }
+
+    return null;
+  }
+
+  public ListNode firstSolution(ListNode head) {
     Set<ListNode> visited = new HashSet<>();
 
     while (head != null) {
