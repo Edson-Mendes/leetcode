20633d62976eb098bbffbfe0f02b722ea43d2f14
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-06-03 12:06:09 -0300

    java: Binary Tree Postorder Traversal - iterative solution

--- java/src/main/java/br/com/emendes/problems/BinaryTreePostorderTraversal.java
+++ java/src/main/java/br/com/emendes/problems/BinaryTreePostorderTraversal.java
@@ -2,8 +2,7 @@ package br.com.emendes.problems;
 
 import br.com.emendes.problems.util.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Dado o root de uma binaru tree, retorne o postorder traversal dos valores dos nodes.<br>
@@ -18,6 +17,24 @@ import java.util.List;
 public class BinaryTreePostorderTraversal {
 
   public List<Integer> postorderTraversal(TreeNode root) {
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    List<Integer> answer = new ArrayList<>();
+    if (root != null) {
+      stack.push(root);
+    }
+    while (!stack.isEmpty()) {
+      TreeNode node = stack.pop();
+      answer.add(node.val);
+      if (node.left != null) stack.push(node.left);
+      if (node.right != null) stack.push(node.right);
+    }
+
+    Collections.reverse(answer);
+
+    return answer;
+  }
+
+  public List<Integer> firstSolution(TreeNode root) {
     List<Integer> answer = new ArrayList<>();
     postorderTraversalHelper(root, answer);
 
