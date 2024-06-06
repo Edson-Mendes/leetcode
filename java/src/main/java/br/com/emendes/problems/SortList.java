b7213338f9e41075c071c4cbd222a4a02d20e33c
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-06-06 17:50:14 -0300

    java: Sort List - merge sort

--- java/src/main/java/br/com/emendes/problems/SortList.java
+++ java/src/main/java/br/com/emendes/problems/SortList.java
@@ -14,6 +14,54 @@ import br.com.emendes.problems.util.ListNode;
 public class SortList {
 
   public ListNode sortList(ListNode head) {
+    if (head == null || head.next == null) return head;
+
+    ListNode slow = head;
+    ListNode fast = head;
+    ListNode mid = head;
+    while (fast != null && fast.next != null) {
+      mid = slow;
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    mid.next = null;
+    head = sortList(head);
+    slow = sortList(slow);
+    return merge(head, slow);
+  }
+
+  private ListNode merge(ListNode h1, ListNode h2) {
+    ListNode dummy = new ListNode(-1);
+    ListNode prev = dummy;
+
+    while (h1 != null && h2 != null) {
+      if (h1.val <= h2.val) {
+        prev.next = h1;
+        h1 = h1.next;
+      } else {
+        prev.next = h2;
+        h2 = h2.next;
+      }
+      prev = prev.next;
+    }
+    while (h1 != null) {
+      prev.next = h1;
+      h1 = h1.next;
+      prev = prev.next;
+    }
+    while (h2 != null) {
+      prev.next = h2;
+      h2 = h2.next;
+      prev = prev.next;
+    }
+
+    return dummy.next;
+  }
+
+  /**
+   * First solution - Insertion sort algorithm.
+   */
+  public ListNode firstSolution(ListNode head) {
     ListNode dummy = new ListNode(-1);
     ListNode curr = head;
     while (curr != null) {
