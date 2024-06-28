da4eb9dfed40f3d5cddee9390b375836ece39fd6
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-06-28 11:52:42 -0300

    java: Maximum Product Subarray - time complexity O(n)

--- java/src/main/java/br/com/emendes/problems/MaximumProductSubarray.java
+++ java/src/main/java/br/com/emendes/problems/MaximumProductSubarray.java
@@ -15,6 +15,28 @@ package br.com.emendes.problems;
 public class MaximumProductSubarray {
 
   public int maxProduct(int[] nums) {
+    int maxProduct = nums[0];
+    int dpMin = nums[0];
+    int dpMax = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+      int num = nums[i];
+      int prevMin = dpMin;
+      int prevMax = dpMax;
+      if (num < 0) {
+        dpMin = Math.min(prevMax * num, num);
+        dpMax = Math.max(prevMin * num, num);
+      } else {
+        dpMin = Math.min(prevMin * num, num);
+        dpMax = Math.max(prevMax * num, num);
+      }
+      maxProduct = Math.max(maxProduct, dpMax);
+    }
+
+    return maxProduct;
+  }
+
+  public int firstSolution(int[] nums) {
     int maxProduct = Integer.MIN_VALUE;
 
     for (int i = 0; i < nums.length; i++) {
