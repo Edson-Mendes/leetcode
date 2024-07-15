34729cea1b4c90469fab2a12acdf70c5923b9bf8
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-07-15 09:38:22 -0300

    java: Find Peak Element - refact

--- java/src/main/java/br/com/emendes/problems/FindPeakElement.java
+++ java/src/main/java/br/com/emendes/problems/FindPeakElement.java
@@ -21,12 +21,9 @@ package br.com.emendes.problems;
 public class FindPeakElement {
 
   public int findPeakElement(int[] nums) {
-    if (nums.length == 1 || nums[0] > nums[1]) return 0;
-    if (nums.length == 2) {
-      return 1;
-    }
-    for (int i = 1; i < nums.length - 1; i++) {
-      if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
+    if (nums.length == 1) return 0;
+    for (int i = 0; i < nums.length - 1; i++) {
+      if (nums[i] > nums[i + 1]) return i;
     }
 
     return nums.length - 1;
