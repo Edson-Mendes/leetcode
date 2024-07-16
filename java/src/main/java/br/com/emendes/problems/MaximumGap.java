51e05b5669edbcedc206e021e048476f64dd5e7f
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-07-16 10:44:31 -0300

    java: Maximum Gap - second solution

--- java/src/main/java/br/com/emendes/problems/MaximumGap.java
+++ java/src/main/java/br/com/emendes/problems/MaximumGap.java
@@ -16,7 +16,50 @@ import java.util.Arrays;
  */
 public class MaximumGap {
 
+  static class Bucket {
+    int min;
+    int max;
+
+    public Bucket(int min, int max) {
+      this.min = min;
+      this.max = max;
+    }
+
+  }
+
   public int maximumGap(int[] nums) {
+    if (nums.length < 2) return 0;
+    int min = Arrays.stream(nums).min().getAsInt();
+    int max = Arrays.stream(nums).max().getAsInt();
+
+    if (min == max) return 0;
+
+    int gap = (int) Math.ceil((double) (max - min) / (nums.length - 1));
+    int bucketLength = (max - min) / gap + 1;
+    Bucket[] buckets = new Bucket[bucketLength];
+
+    for (int i = 0; i < bucketLength; i++) {
+      buckets[i] = new Bucket(Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+    for (int num : nums) {
+      int i = (num - min) / gap;
+      buckets[i].min = Math.min(buckets[i].min, num);
+      buckets[i].max = Math.max(buckets[i].max, num);
+    }
+
+    int answer = 0;
+    int prevMax = min;
+    for (Bucket bucket : buckets) {
+      if (bucket.min == Integer.MAX_VALUE) continue;
+
+      answer = Math.max(answer, bucket.min - prevMax);
+      prevMax = bucket.max;
+    }
+
+    return answer;
+  }
+
+  public int firstSolution(int[] nums) {
     Arrays.sort(nums);
     int answer = 0;
     for (int i = 0; i < nums.length - 1; i++) {
