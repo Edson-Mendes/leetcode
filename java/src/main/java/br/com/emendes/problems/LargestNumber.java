5877c9ed3121a1b38b7799777f1a8c3b841f68d2
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-08-07 14:52:51 -0300

    java: Largest Number - second solution

--- java/src/main/java/br/com/emendes/problems/LargestNumber.java
+++ java/src/main/java/br/com/emendes/problems/LargestNumber.java
@@ -15,6 +15,21 @@ import java.util.stream.Collectors;
 public class LargestNumber {
 
   public String largestNumber(int[] nums) {
+    String[] numbers = new String[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      numbers[i] = String.valueOf(nums[i]);
+    }
+    Arrays.sort(numbers, (n1, n2) -> (n2 + n1).compareTo(n1 + n2));
+
+    String answer = "";
+    for (String number : numbers) {
+      answer += number;
+    }
+
+    return answer.startsWith("00") ? "0" : answer;
+  }
+
+  public String firstSolution(int[] nums) {
     String answer = Arrays.stream(nums)
         .mapToObj(String::valueOf)
         .sorted((n1, n2) -> (n2 + n1).compareTo(n1 + n2))
