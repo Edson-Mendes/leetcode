37be347ed27bdff7ec34348887e85e60c7de75bc
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-08-22 14:29:25 -0300

    mysql: Department Highest Salary - second solution

--- sql/mysql/problems/DepartmentHighestSalary.sql
+++ sql/mysql/problems/DepartmentHighestSalary.sql
@@ -14,10 +14,19 @@ CREATE TABLE Department (
   name varchar(255)
 );
 
--- Solution
+-- First Solution
 WITH highestSalaryByDepartament AS (SELECT d.id AS 'departmentId', MAX(e.salary) AS 'salary' FROM Employee e 
 	JOIN Department d ON e.departmentId = d.id
 	GROUP BY d.id)
 SELECT d.name AS 'Department', e.name AS 'Employee', h.salary AS 'Salary' 
   FROM Employee e, highestSalaryByDepartament h, Department d
 	WHERE h.departmentId = e.departmentId AND e.salary = h.salary AND h.departmentId = d.id;
+
+-- Second Solution
+WITH highestSalaryByDepartament AS (SELECT d.id AS 'departmentId', MAX(e.salary) AS 'salary' FROM Employee e 
+	JOIN Department d ON e.departmentId = d.id
+	GROUP BY d.id)
+SELECT d.name AS 'Department', e.name AS 'Employee', h.salary AS 'Salary' 
+	FROM Employee e
+	JOIN highestSalaryByDepartament h ON e.departmentId = h.departmentId AND e.salary = h.salary
+	JOIN Department d ON h.departmentId = d.id;
