61f39b232f436d6303e6c9fedab778307c3fcc82
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-08-29 10:44:21 -0300

    mysql: Delete Duplicate Emails - second solution

--- sql/mysql/problems/DeleteDuplicateEmails.sql
+++ sql/mysql/problems/DeleteDuplicateEmails.sql
@@ -7,8 +7,11 @@ CREATE TABLE Person(
   Email varchar(255)
 );
 
--- Solution
+-- First solution
 WITH emailRanking AS (
 	SELECT p.id, p.email, DENSE_RANK() OVER(PARTITION BY email ORDER BY id) AS 'rank' FROM Person p
 ) 
 DELETE FROM Person p WHERE p.id IN (SELECT er.id FROM emailRanking er WHERE er.rank != 1);
+
+-- Second solution
+DELETE p1 FROM Person p1 JOIN Person p2 ON p1.email = p2.email AND p1.id > p2.id;
