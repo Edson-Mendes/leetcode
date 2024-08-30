4499a499c8559bcf8ec6143116cdfe8fadd4b7bb
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-08-30 10:24:19 -0300

    java: House Robber - second solution

--- java/src/main/java/br/com/emendes/problems/HouseRobber.java
+++ java/src/main/java/br/com/emendes/problems/HouseRobber.java
@@ -19,6 +19,21 @@ public class HouseRobber {
   public int rob(int[] nums) {
     if (nums.length == 1) return nums[0];
 
+    int prev1 = Math.max(nums[0], nums[1]);
+    int prev2 = nums[0];
+
+    for (int i = 2; i < nums.length; i++) {
+      int dp = Math.max(prev1, prev2 + nums[i]);
+      prev2 = prev1;
+      prev1 = dp;
+    }
+
+    return prev1;
+  }
+
+  public int firstSolution(int[] nums) {
+    if (nums.length == 1) return nums[0];
+
     int[] dp = new int[nums.length];
     dp[0] = nums[0];
     dp[1] = Math.max(nums[0], nums[1]);
