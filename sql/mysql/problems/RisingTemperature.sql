aa92092877fb82c7306af2cc2b4381a8523b808d
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-08-30 08:27:29 -0300

    mysql: Rising Temperature

---
+++ sql/mysql/problems/RisingTemperature.sql
@@ -0,0 +1,15 @@
+-- Escreva um solução para encontrar todos os ids das data onde a temperatura é maior 
+-- comparada ao dia anterior.
+-- Retorne a tabela resultado em qualquer ordem.
+
+-- Create tables
+CREATE TABLE Weather(
+	id int, 
+	recordDate date, 
+	temperature int
+);
+
+-- Solution
+SELECT w1.id FROM Weather w1, Weather w2 
+  WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 
+  AND w1.temperature > w2.temperature;
