7eeb416c821ff41804343d0a3dfdb97e27cc0c35
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-08-30 10:16:15 -0300

    java: House Robber - refact

--- java/src/main/java/br/com/emendes/problems/HouseRobber.java
+++ java/src/main/java/br/com/emendes/problems/HouseRobber.java
@@ -18,32 +18,16 @@ public class HouseRobber {
 
   public int rob(int[] nums) {
     if (nums.length == 1) return nums[0];
-    if (nums.length == 2) return Math.max(nums[0], nums[1]);
 
-    int[][] dp = new int[nums.length][2];
-
-    dp[0][0] = nums[0];
-    dp[0][1] = 1;
-    dp[1][0] = Math.max(nums[0], nums[1]);
-    dp[1][1] = dp[1][0] == nums[1] ? 1 : 0;
+    int[] dp = new int[nums.length];
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
 
     for (int i = 2; i < nums.length; i++) {
-      if (nums[i] == 0) {
-        dp[i][0] = dp[i - 1][0];
-        continue;
-      }
-      if (dp[i - 1][1] == 0) {
-        dp[i][0] = dp[i - 1][0] + nums[i];
-        dp[i][1] = 1;
-      } else if (dp[i - 1][0] > dp[i - 2][0] + nums[i]) {
-        dp[i][0] = dp[i - 1][0];
-      } else {
-        dp[i][0] = dp[i - 2][0] + nums[i];
-        dp[i][1] = 1;
-      }
+      dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
     }
 
-    return dp[nums.length - 1][0];
+    return dp[nums.length - 1];
   }
 
 }
