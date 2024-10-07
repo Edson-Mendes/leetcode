fed358c69b0b8581de6c0d2adb3597959ceb4291
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-10-07 11:10:36 -0300

    java: Contains Duplicate II -  second solution

--- java/src/main/java/br/com/emendes/problems/ContainsDuplicateII.java
+++ java/src/main/java/br/com/emendes/problems/ContainsDuplicateII.java
@@ -1,7 +1,9 @@
 package br.com.emendes.problems;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Dado um array de inteiros nums e um inteiro k, retorne true se existe dois índices distintos i e j no array
@@ -15,6 +17,15 @@ import java.util.Map;
 public class ContainsDuplicateII {
 
   public boolean containsNearbyDuplicate(int[] nums, int k) {
+    Set<Integer> seen = new HashSet<>();
+    for (int i = 0; i < nums.length; i++) {
+      if (!seen.add(nums[i])) return true;
+      if (i >= k) seen.remove(nums[i - k]);
+    }
+    return false;
+  }
+
+  public boolean firstSolution(int[] nums, int k) {
     Map<Integer, Integer> numberToIndex = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
       if (numberToIndex.containsKey(nums[i]) && (Math.abs(numberToIndex.get(nums[i]) - i) <= k)) return true;
