e8ae1361e50ecdf64bf319f584e4f9338419374f
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-10-08 10:44:57 -0300

    java: Contains Duplicate III - Tree Set

--- java/src/main/java/br/com/emendes/problems/ContainsDuplicateIII.java
+++ java/src/main/java/br/com/emendes/problems/ContainsDuplicateIII.java
@@ -2,6 +2,7 @@ package br.com.emendes.problems;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Dado uma array de inteiros nums e dois inteiros indexDiff e valueDiff.<br>
@@ -22,6 +23,23 @@ import java.util.Set;
 public class ContainsDuplicateIII {
 
   public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+    TreeSet<Long> seen = new TreeSet<>();
+    for (int i = 0; i < nums.length; i++) {
+      long num = nums[i];
+      Long ceiling = seen.ceiling(num);
+      if (ceiling != null && ceiling - num <= valueDiff) return true;
+      Long floor = seen.floor(num);
+      if (floor != null && num - floor <= valueDiff) return true;
+
+      seen.add(num);
+      if (i >= indexDiff) {
+        seen.remove((long) nums[i - indexDiff]);
+      }
+    }
+    return false;
+  }
+
+  public boolean firstSolution(int[] nums, int indexDiff, int valueDiff) {
     Set<Integer> seen = new HashSet<>();
     for (int i = 0; i < nums.length; i++) {
       for (int value : seen) {
