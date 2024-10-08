65fc221020e6076d5e659f1aa6188133013f4745
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-10-08 16:23:51 -0300

    java: Count Complete Tree Node - second solution

--- java/src/main/java/br/com/emendes/problems/CountCompleteTreeNodes.java
+++ java/src/main/java/br/com/emendes/problems/CountCompleteTreeNodes.java
@@ -19,6 +19,30 @@ import br.com.emendes.problems.util.TreeNode;
 public class CountCompleteTreeNodes {
 
   public int countNodes(TreeNode root) {
+    if (root == null)
+      return 0;
+
+    TreeNode left = root;
+    TreeNode right = root;
+    int heightL = 0;
+    int heightR = 0;
+
+    while (left != null) {
+      heightL++;
+      left = left.left;
+    }
+
+    while (right != null) {
+      heightR++;
+      right = right.right;
+    }
+
+    if (heightL == heightR)
+      return (int) Math.pow(2, heightL) - 1;
+    return 1 + countNodes(root.left) + countNodes(root.right);
+  }
+
+  public int firstSolution(TreeNode root) {
     if (root == null) return 0;
 
     return 1 + countNodes(root.left) + countNodes(root.right);
