e14167560ea06900149cdcf218f8cdd10b176d73
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-10-11 08:58:54 -0300

    javascript: Palindrome Number - second solution

--- javascript/problems/PalindromeNumber.js
+++ javascript/problems/PalindromeNumber.js
@@ -10,6 +10,16 @@
  */
 var isPalindrome = function (x) {
   let xString = x.toString();
+  let right = xString.length - 1;
+  let left = 0;
+  while (left <= right) {
+    if (xString[left++] != xString[right--]) return false;
+  }
+  return true;
+};
+
+var firstSolution = function (x) {
+  let xString = x.toString();
   let xInverted = "";
   for (var i = xString.length - 1; i >= 0; i--) {
     xInverted += xString[i];
