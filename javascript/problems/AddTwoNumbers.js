8066d20897674c2001dc29a08a2a3d85f1876fea
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-10-11 14:14:11 -0300

    javascript: Add Two Number - refact

--- javascript/problems/AddTwoNumbers.js
+++ javascript/problems/AddTwoNumbers.js
@@ -20,36 +20,26 @@
 var addTwoNumbers = function (l1, l2) {
   let dummy = { val: 0, next: null };
   let result = dummy;
-  let remainder = 0;
-  while (l1 && l2) {
-    let val = l1.val + l2.val + remainder;
-    remainder = Math.floor(val / 10);
-    val = val % 10;
-    result.next = { val: val, next: null };
+  let carry = 0;
+  while (l1 || l2 || carry > 0) {
+    let sum = 0;
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+    sum += carry;
+    carry = 0;
+    if (sum > 9) {
+      carry = 1;
+      sum -= 10;
+    }
+    result.next = new ListNode(sum);
     result = result.next;
-    l1 = l1.next;
-    l2 = l2.next;
   }
-  while (l1) {
-    let val = l1.val + remainder;
-    remainder = Math.floor(val / 10);
-    val = val % 10;
-    result.next = { val: val, next: null };
-    result = result.next;
-    l1 = l1.next;
-  }
-  while (l2) {
-    let val = l2.val + remainder;
-    remainder = Math.floor(val / 10);
-    val = val % 10;
-    result.next = { val: val, next: null };
-    result = result.next;
-    l2 = l2.next;
-  }
-  if (remainder > 0) {
-    result.next = {val: remainder, next: null};
-  }
-
   return dummy.next;
 };
 
@@ -63,3 +53,8 @@ const generateListNode = (values) => {
 
   return dummy.next;
 };
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
