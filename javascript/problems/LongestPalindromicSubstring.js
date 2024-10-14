d1b117da645381ef06cceedf4f6bf7212d13c0d5
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-10-14 10:36:55 -0300

    javascript: Longest Palindromic Substring - second solution

--- javascript/problems/LongestPalindromicSubstring.js
+++ javascript/problems/LongestPalindromicSubstring.js
@@ -11,6 +11,34 @@
  * @return {string}
  */
 var longestPalindrome = function (s) {
+  let start = 0;
+  let end = 0;
+
+  const expandAroundCenter = (s, left, right) => {
+    while (left > -1 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+
+    return right - left - 1;
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    let len1 = expandAroundCenter(s, i, i);
+    let len2 = expandAroundCenter(s, i, i + 1);
+
+    let len = Math.max(len1, len2);
+
+    if (len > end - start + 1) {
+      start = i - Math.floor((len - 1) / 2);
+      end = i + Math.floor(len / 2);
+    }
+  }
+
+  return s.substring(start, end + 1);
+};
+
+var firstSolution = function (s) {
   let cache = Array(s.length)
     .fill()
     .map(() => Array(s.length).fill(undefined));
@@ -35,5 +63,3 @@ const dp = (s, i, j, cache) => {
   cache[i][j] = s[i] === s[j] && dp(s, i + 1, j - 1, cache);
   return cache[i][j];
 };
-
-console.log(longestPalindrome("Aaa"));
