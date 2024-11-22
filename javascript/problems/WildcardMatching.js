b7703d3e3e531b98744df07da5dd9a753f08c08c
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-11-22 10:19:07 -0300

    javascript: Wildcard Matching - second solution

--- javascript/problems/WildcardMatching.js
+++ javascript/problems/WildcardMatching.js
@@ -21,10 +21,47 @@
  * @return {boolean}
  */
 const isMatch = function (s, p) {
-  return matches(s, p, 0, 0, new Map());
+  const sLen = s.length;
+  const pLen = p.length;
+  const cache = createCache(sLen + 1, pLen + 1);
+  cache[0][0] = true;
+  for (let pi = 0; pi < pLen; pi++) {
+    if (p[pi] === "*") cache[0][pi + 1] = cache[0][pi];
+  }
+  for (let si = 0; si < sLen; si++) {
+    for (let pi = 0; pi < pLen; pi++) {
+      if (p[pi] === "*") {
+        cache[si + 1][pi + 1] = cache[si + 1][pi] || cache[si][pi + 1];
+      } else if (matches(s, p, si, pi)) {
+        cache[si + 1][pi + 1] = cache[si][pi];
+      }
+    }
+  }
+
+  return cache[sLen][pLen];
+}
+
+const matches = (str, pat, si, pi) => {
+  return pat[pi] === "?" || str[si] === pat[pi];
+}
+
+const createCache = (lines, columns) => {
+  const cache = new Array(lines);
+  for (let i = 0; i < lines; i++) {
+    cache[i] = new Array(columns).fill(false);
+  }
+
+  return cache;
+}
+
+/**
+ * First Solution.
+ */
+const firstSolution = function (s, p) {
+  return matchesFirstSolution(s, p, 0, 0, new Map());
 };
 
-const matches = (str, pat, si, pi, cache) => {
+const matchesFirstSolution = (str, pat, si, pi, cache) => {
   const key = si + "-" + pi;
   if (cache.has(key)) return cache.get(key);
   if (pi === pat.length) {
@@ -33,16 +70,16 @@ const matches = (str, pat, si, pi, cache) => {
     let value;
     switch (pat[pi]) {
       case "?":
-        value = si < str.length ? matches(str, pat, si + 1, pi + 1, cache) : false;
+        value = si < str.length ? matchesFirstSolution(str, pat, si + 1, pi + 1, cache) : false;
         break;
       case "*":
         value = si < str.length
-            ? matches(str, pat, si, pi + 1, cache) || matches(str, pat, si + 1, pi, cache)
-            : matches(str, pat, si, pi + 1, cache);
+            ? matchesFirstSolution(str, pat, si, pi + 1, cache) || matchesFirstSolution(str, pat, si + 1, pi, cache)
+            : matchesFirstSolution(str, pat, si, pi + 1, cache);
         break;
       default:
         value = si < str.length && str[si] === pat[pi]
-            ? matches(str, pat, si + 1, pi + 1, cache)
+            ? matchesFirstSolution(str, pat, si + 1, pi + 1, cache)
             : false;
     }
     cache.set(key, value);
