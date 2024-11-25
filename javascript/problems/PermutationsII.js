47b9d97f0ea8cc6b3703ce92e05a34810054db24
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-11-25 13:20:57 -0300

    javascript: Permutations II - second solution

--- javascript/problems/PermutationsII.js
+++ javascript/problems/PermutationsII.js
@@ -13,13 +13,40 @@
  */
 const permuteUnique = function (nums) {
   const permutations = [];
-  const values = toMap(nums);
-  generatePermutations(values, nums.length, [], permutations);
+  nums.sort((a, b) => a - b);
+  generatePermutations(nums, [], [], permutations);
 
   return permutations;
 };
 
-const generatePermutations = (values, length, permutation, permutations) => {
+const generatePermutations = (nums, used, permutation, permutations) => {
+  if (permutation.length === nums.length) {
+    permutations.push([...permutation]);
+    return;
+  }
+  for (let index = 0; index < nums.length; index++) {
+    if (used[index]) continue;
+    if (nums[index] === nums[index - 1] && !used[index - 1]) continue;
+    used[index] = true;
+    permutation.push(nums[index]);
+    generatePermutations(nums, used, permutation, permutations);
+    permutation.pop();
+    used[index] = false;
+  }
+};
+
+/**
+ * First Solution.
+ */
+const firstSolution = function (nums) {
+  const permutations = [];
+  const values = toMap(nums);
+  generatePermutations2(values, nums.length, [], permutations);
+
+  return permutations;
+};
+
+const generatePermutations2 = (values, length, permutation, permutations) => {
   if (permutation.length === length) {
     permutations.push([...permutation]);
     return;
@@ -28,7 +55,7 @@ const generatePermutations = (values, length, permutation, permutations) => {
     if (pair[1] > 0) {
       permutation.push(pair[0]);
       values.set(pair[0], pair[1] - 1);
-      generatePermutations(values, length, permutation, permutations);
+      generatePermutations2(values, length, permutation, permutations);
       values.set(pair[0], pair[1]);
       permutation.pop();
     }
