7791ccffdb6244ca8034e8b5436f0bc69b6d7812
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-12-02 16:29:44 -0300

    javascript: Jump Game

---
+++ javascript/problems/JumpGame.js
@@ -0,0 +1,27 @@
+/**
+ * Você recebe um array de inteiros nums. Você esta possicionado no primeiro índice do array,
+ * e cada elemento do array representa o máximo comprimento do seu pulo a partir daquela possição.
+ *
+ * Retorne true se você pode alcançar o último índice, ou false caso contrário.
+ *
+ * Restrições:
+ *
+ * 1 <= nums.length <= 10⁴
+ *
+ * 0 <= nums[i] <= 10⁵
+ *
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const canJump = function (nums) {
+  if (nums.length === 1) return true;
+  let index = nums.length - 2;
+  let jump = 1;
+  while (index > 0) {
+    if (nums[index] >= jump) jump = 1;
+    else jump++;
+    index--;
+  }
+
+  return nums[0] >= jump;
+};
