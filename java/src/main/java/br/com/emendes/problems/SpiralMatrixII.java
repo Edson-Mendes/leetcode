c02f6249a47615598fc72913d6bc0887bbb24c13
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-12-04 10:27:37 -0300

    java: Spiral Matrix - refact

--- java/src/main/java/br/com/emendes/problems/SpiralMatrixII.java
+++ java/src/main/java/br/com/emendes/problems/SpiralMatrixII.java
@@ -11,30 +11,30 @@ public class SpiralMatrixII {
   public int[][] generateMatrix(int n) {
     int[][] answer = new int[n][n];
     int maxElements = n * n;
-    int counter = 0;
+    int counter = 1;
 
     int iStart = 0;
     int iEnd = n - 1;
     int jStart = 0;
     int jEnd = iEnd;
 
-    while (counter < maxElements) {
+    while (counter <= maxElements) {
       for (int j = jStart; j <= jEnd; j++) {
-        answer[iStart][j] = ++counter;
-      }
-      for (int i = iStart + 1; i <= iEnd && counter < maxElements; i++) {
-        answer[i][jEnd] = ++counter;
-      }
-      for (int j = jEnd - 1; j >= jStart && counter < maxElements; j--) {
-        answer[iEnd][j] = ++counter;
-      }
-      for (int i = iEnd - 1; i >= iStart + 1 && counter < maxElements; i--) {
-        answer[i][jStart] = ++counter;
+        answer[iStart][j] = counter++;
       }
       iStart++;
-      jStart++;
-      iEnd--;
+      for (int i = iStart; i <= iEnd; i++) {
+        answer[i][jEnd] = counter++;
+      }
       jEnd--;
+      for (int j = jEnd; j >= jStart; j--) {
+        answer[iEnd][j] = counter++;
+      }
+      iEnd--;
+      for (int i = iEnd; i >= iStart; i--) {
+        answer[i][jStart] = counter++;
+      }
+      jStart++;
     }
 
     return answer;
