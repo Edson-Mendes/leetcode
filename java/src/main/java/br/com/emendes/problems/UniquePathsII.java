b4895b09954be2cf5cd586305583c74f6543bb2a
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-12-06 13:56:04 -0300

    java: Unique Paths II - refact

--- java/src/main/java/br/com/emendes/problems/UniquePathsII.java
+++ java/src/main/java/br/com/emendes/problems/UniquePathsII.java
@@ -23,43 +23,19 @@ public class UniquePathsII {
   public int uniquePathsWithObstacles(int[][] obstacleGrid) {
     int m = obstacleGrid.length;
     int n = obstacleGrid[0].length;
+    if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) return 0;
 
-    // caso base.
-    if (m == 1) {
-      for (int j = 0; j < n; j++) {
-        if (obstacleGrid[0][j] == 1) return 0;
-      }
-      return 1;
-    }
-    if (n == 1) {
-      for (int i = 0; i < m; i++) {
-        if (obstacleGrid[i][0] == 1) return 0;
-      }
-      return 1;
-    }
+    obstacleGrid[0][0] = 1;
+    for (int j = 1; j < n; j++)
+      obstacleGrid[0][j] = obstacleGrid[0][j] == 1 ? 0 : obstacleGrid[0][j - 1];
+    for (int i = 1; i < m; i++)
+      obstacleGrid[i][0] = obstacleGrid[i][0] == 1 ? 0 : obstacleGrid[i - 1][0];
 
-    int[][] buffer = new int[m][n];
+    for (int i = 1; i < m; i++)
+      for (int j = 1; j < n; j++)
+        obstacleGrid[i][j] = obstacleGrid[i][j] == 1 ? 0 : obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
 
-    // preencher a linha.
-    int value = 1;
-    for (int j = 0; j < n; j++) {
-      if (obstacleGrid[0][j] == 1) value = 0;
-      buffer[0][j] = value;
-    }
-    // preencher coluna.
-    value = 1;
-    for (int i = 0; i < m; i++) {
-      if (obstacleGrid[i][0] == 1) value = 0;
-      buffer[i][0] = value;
-    }
-
-    for (int i = 1; i < m; i++) {
-      for (int j = 1; j < n; j++) {
-        buffer[i][j] = obstacleGrid[i][j] == 1 ? 0 : buffer[i - 1][j] + buffer[i][j - 1];
-      }
-    }
-
-    return buffer[m - 1][n - 1];
+    return obstacleGrid[m - 1][n - 1];
   }
 
 }
