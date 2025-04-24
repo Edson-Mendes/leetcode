dd8864585e930140c838c1318cb5e747383d62c7
Edson Mendes <edson.luiz.mendes@hotmail.com>
2025-04-24 09:33:19 -0300

    javascript: Set Matrix Zeroes - second solution

--- javascript/problems/SetMatrixZeroes.js
+++ javascript/problems/SetMatrixZeroes.js
@@ -16,7 +16,7 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-const setZeroes = function (matrix) {
+const firstSolution = function (matrix) {
   const lines = new Set();
   const columns = new Set();
   const m = matrix.length;
@@ -40,4 +40,41 @@ const setZeroes = function (matrix) {
       matrix[i][column] = 0;
     }
   }
+};
+
+const setZeroes = function (matrix) {
+  const m = matrix.length;
+  const n = matrix[0].length;
+  let firstLine = false;
+  let firstColumn = false;
+
+  for (let j = 0; j < n && !firstLine; j++) {
+    if (matrix[0][j] === 0) firstLine = true;
+  }
+  for (let i = 0; i < m && !firstColumn; i++) {
+    if (matrix[i][0] === 0) firstColumn = true;
+  }
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[0][j] = 0;
+        matrix[i][0] = 0;
+      }
+    }
+  }
+
+  for (let i = 1; i < m; i++) {
+    if (matrix[i][0] === 0)
+      for (let j = 1; j < n; j++) {
+        matrix[i][j] = 0;
+      }
+  }
+  for (let j = 1; j < n; j++) {
+    if (matrix[0][j] === 0)
+      for (let i = 1; i < m; i++) {
+        matrix[i][j] = 0;
+      }
+  }
+  if (firstLine) for (let j = 0; j < n; j++) matrix[0][j] = 0;
+  if (firstColumn) for (let i = 0; i < m; i++) matrix[i][0] = 0;
 };
