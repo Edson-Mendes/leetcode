df4ff259358b878c21c485b9a5de6f646b3f0fac
Edson Mendes <edson.luiz.mendes@hotmail.com>
2025-04-24 11:27:35 -0300

    java: Set Matrix Zeroes - second solution

--- java/src/main/java/br/com/emendes/problems/SetMatrixZeroes.java
+++ java/src/main/java/br/com/emendes/problems/SetMatrixZeroes.java
@@ -16,6 +16,47 @@ import java.util.Set;
 public class SetMatrixZeroes {
 
   public void setZeroes(int[][] matrix) {
+    boolean firstRow = false;
+    boolean firstColumn = false;
+    int m = matrix.length;
+    int n = matrix[0].length;
+
+    for (int i = 0; i < m; i++)
+      if (matrix[i][0] == 0) {
+        firstColumn = true;
+        break;
+      }
+    for (int j = 0; j < n; j++)
+      if (matrix[0][j] == 0) {
+        firstRow = true;
+        break;
+      }
+
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        if (matrix[i][j] == 0) {
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
+        }
+      }
+    }
+    for (int i = 1; i < m; i++)
+      if (matrix[i][0] == 0)
+        for (int j = 1; j < n; j++)
+          matrix[i][j] = 0;
+    for (int j = 1; j < n; j++)
+      if (matrix[0][j] == 0)
+        for (int i = 1; i < m; i++)
+          matrix[i][j] = 0;
+    if (firstRow)
+      for (int j = 0; j < n; j++)
+        matrix[0][j] = 0;
+    if (firstColumn)
+      for (int i = 1; i < m; i++)
+        matrix[i][0] = 0;
+  }
+
+  public void firstSolution(int[][] matrix) {
     int m = matrix.length;
     int n = matrix[0].length;
 
@@ -30,19 +71,16 @@ public class SetMatrixZeroes {
         }
       }
     }
-
     for (int row : rows) {
       for (int j = 0; j < n; j++) {
         matrix[row][j] = 0;
       }
     }
-
     for (int column : columns) {
       for (int i = 0; i < m; i++) {
         matrix[i][column] = 0;
       }
     }
-
   }
 
 }
