8e744d39c3ff854376677594f4c2dfc9eeb18ec6
Edson Mendes <edson.luiz.mendes@hotmail.com>
2025-05-05 12:07:31 -0300

    javascript: Minimum Window Substring

---
+++ javascript/problems/MinimumWindowSubstring.js
@@ -0,0 +1,50 @@
+/**
+ * Dado duas strings s e t de comprimentos m e n respectivamente, retorne a substring de janela mínima
+ * de s de modo que todos os elementos em t (incluindo duplicatas) estejam inclusos na janela. Se não existe
+ * tal substring, retorne uma string vazia.
+ *
+ * Os casos de testes serão gerados de modo que a resposta seja única.
+ *
+ * Restrições:
+ *
+ * m == s.length
+ *
+ * n = t.length
+ *
+ * 1 <= m, n <= 10⁵
+ *
+ * s e t consistem de letras maiúsculas.
+ */
+const minWindow = function (s, t) {
+  let answer = [0, 1000000];
+  const tMap = toMap(t);
+  let counter = t.length;
+  let l = 0;
+  let i = 0;
+  const queue = [];
+  while (i < s.length && !tMap[s[i]]) i++;
+  l = i;
+  while (i < s.length) {
+    if (tMap[s[i]] !== undefined) {
+      if (i !== l) queue.push(i);
+      if (tMap[s[i]]-- > 0) counter--;
+      while (counter === 0) {
+        if (answer[1] - answer[0] + 1 > i - l + 1) answer = [l, i];
+        if (++tMap[s[l]] > 0) counter++;
+        l = queue.shift();
+      }
+    }
+    i++;
+  }
+  return answer[1] === 1000000 ? "" : s.substring(answer[0], answer[1] + 1);
+};
+
+const toMap = (string) => {
+  const map = {};
+  for (const character of string) {
+    if (map[character]) {
+      map[character]++;
+    } else map[character] = 1;
+  }
+  return map;
+};
