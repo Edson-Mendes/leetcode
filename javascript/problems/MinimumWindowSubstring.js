20f00b490bcbaf64f71c420b8a427ad9446aa18d
Edson Mendes <edson.luiz.mendes@hotmail.com>
2025-05-06 13:33:58 -0300

    javascript: Minimum Window Substring - second solution

--- javascript/problems/MinimumWindowSubstring.js
+++ javascript/problems/MinimumWindowSubstring.js
@@ -16,6 +16,27 @@
  * s e t consistem de letras maiúsculas.
  */
 const minWindow = function (s, t) {
+  const counter = new Array(128).fill(0);
+  for (let index = 0; index < t.length; index++) counter[t.charCodeAt(index)]++;
+  let required = t.length;
+  let start = -1;
+  let end = s.length;
+  for (let l = 0, r = 0; r < s.length; r++) {
+    if (--counter[s.charCodeAt(r)] >= 0) required--;
+    while (required === 0) {
+      if (r - l + 1 < end - start) {
+        start = l;
+        end = r + 1;
+      }
+      if (++counter[s.charCodeAt(l)] > 0) required++;
+      l++;
+    }
+  }
+
+  return start === -1 ? "" : s.substring(start, end);
+};
+
+const firstSolution = function (s, t) {
   let answer = [0, 1000000];
   const tMap = toMap(t);
   let counter = t.length;
