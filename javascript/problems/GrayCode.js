44b044629afd99d32f6dd927abba2036aaaa85e2
Edson Mendes <edson.luiz.mendes@hotmail.com>
2025-05-17 19:08:33 -0300

    javascript: Gray Code

---
+++ javascript/problems/GrayCode.js
@@ -0,0 +1,40 @@
+/**
+ * Um n-bit gray code sequence é uma sequência de 2^n inteiros onde:
+ *
+ * - Cada inteiro está no intervalo [0, 2^n - 1]
+ *
+ * - O primeiro inteiro é 0
+ *
+ * - Um inteiro não aparece mais do que uma vez
+ *
+ * - A representação binária de cada par adjacente difere em exatos 1 bit
+ *
+ * - A representação binária do primeiro e último inteiros difere em exatos 1 bit
+ *
+ * Dado um inteiro n, retorne qualquer n-bit gray code sequence.
+ *
+ * Restrições:
+ *
+ * - 1 <= n <= 16
+ */
+const grayCode = function (n) {
+  let total = Math.pow(2, n) - 1;
+  const sequence = [0];
+  const memo = new Set();
+  memo.add(0);
+  grayCodeHelper(sequence, memo, total);
+
+  return sequence;
+};
+
+const grayCodeHelper = (sequence, memo, count) => {
+  if (count === 0) return;
+  const last = sequence[sequence.length - 1];
+  let i = 1;
+  while (memo.has(last ^ i)) {
+    i <<= 1;
+  }
+  sequence.push(last ^ i);
+  memo.add(last ^ i);
+  grayCodeHelper(sequence, memo, count - 1);
+};
