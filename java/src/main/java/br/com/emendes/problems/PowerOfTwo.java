58d02c89fa6e57659f1345deb041a549f022a3a9
Edson Mendes <edson.luiz.mendes@hotmail.com>
2025-05-31 10:37:31 -0300

    java: Power of Two - second solution

--- java/src/main/java/br/com/emendes/problems/PowerOfTwo.java
+++ java/src/main/java/br/com/emendes/problems/PowerOfTwo.java
@@ -14,6 +14,15 @@ public class PowerOfTwo {
 
   public boolean isPowerOfTwo(int n) {
     if (n <= 0) return false;
+    while ((n & 1) != 1) {
+      n >>= 1;
+    }
+    n >>= 1;
+    return n == 0;
+  }
+
+  public boolean firstSolution(int n) {
+    if (n <= 0) return false;
     while (n % 2 != 1) {
       n /= 2;
     }
