3296cc7cec6513cc4dc25f90f61fe44268bf6b5c
Edson Mendes <edson.luiz.mendes@hotmail.com>
2025-09-14 17:48:27 -0300

    java: Palindrome Linked List - second solution

--- java/src/main/java/br/com/emendes/problems/PalindromeLinkedList.java
+++ java/src/main/java/br/com/emendes/problems/PalindromeLinkedList.java
@@ -11,10 +11,43 @@ import java.util.Deque;
  * Restrições:<br>
  * o número de nodes na lista está no intervalo [1, 10⁵].<br>
  * 0 <= Node.val <= 9
+ * <br><br>
+ * Follow up: Você poderia resolver com complexidade de tempo O(n) e complexidade de espaço O(1)?
  */
 public class PalindromeLinkedList {
 
   public boolean isPalindrome(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    if (fast != null)
+      slow = slow.next;
+    slow = reverseList(slow);
+
+    while (slow != null) {
+      if (slow.val != head.val) return false;
+      head = head.next;
+      slow = slow.next;
+    }
+    return true;
+  }
+
+  private ListNode reverseList(ListNode head) {
+    ListNode prev = null;
+    while (head != null) {
+      ListNode next = head.next;
+      head.next = prev;
+      prev = head;
+      head = next;
+    }
+
+    return prev;
+  }
+
+  public boolean firstSolution(ListNode head) {
     Deque<Integer> stack = new ArrayDeque<>();
 
     ListNode node = head;
