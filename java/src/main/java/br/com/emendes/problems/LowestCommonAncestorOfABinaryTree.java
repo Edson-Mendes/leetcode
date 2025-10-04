2451ef5550c389fe02516c665c645ff87c141fc3
Edson Mendes <edson.luiz.mendes@hotmail.com>
2025-10-04 17:55:46 -0300

    java: Lowest Common Ancestor of a Binary Tree - second solution

--- java/src/main/java/br/com/emendes/problems/LowestCommonAncestorOfABinaryTree.java
+++ java/src/main/java/br/com/emendes/problems/LowestCommonAncestorOfABinaryTree.java
@@ -21,6 +21,16 @@ import java.util.Deque;
 public class LowestCommonAncestorOfABinaryTree {
 
   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root == null || root.val == p.val || root.val == q.val)
+      return root;
+    TreeNode left = lowestCommonAncestor(root.left, p, q);
+    TreeNode right = lowestCommonAncestor(root.right, p, q);
+    if (left != null && right != null)
+      return root;
+    return left == null ? right : left;
+  }
+
+  public TreeNode firstSolution(TreeNode root, TreeNode p, TreeNode q) {
     Deque<TreeNode> pPath = new ArrayDeque<>();
     Deque<TreeNode> qPath = new ArrayDeque<>();
     getPath(root, p, pPath);
